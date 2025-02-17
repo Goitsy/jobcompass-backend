3b4aa5b91fc6665420a1ac20ed39dde0130878e7
GDMontsho <montshogoitseone@gmail.com>
2025-02-17 10:14:35 +0100

    update urls

--- server.js
+++ server.js
@@ -18,11 +18,7 @@ connect();
 app.use(express.json());
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",
-      "http://localhost:5174",
-      "http://localhost:5175",
-    ],
+    origin: ["https://jobcompass-frontend.onrender.com"],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
